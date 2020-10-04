6603f4c37d214d502b48d06605f23dadafa98198
17543268 <Mosin.M.Yu@sberbank.ru>
2020-10-05 01:18:04 +0300

    Added new reducer for work with phones ids

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import {connectRouter} from "connected-react-router";
 import phones from "./phones";
+import phonesPage from "./phonesPage";
 
 export default history => combineReducers({
     phones,
+    phonesPage,
     router: connectRouter(history)
 })
