0e0b938b28e75884fd5e418a74779d8c6360deca
17543268 <Mosin.M.Yu@sberbank.ru>
2020-10-05 00:58:22 +0300

    Added fetchPhones api, reducer, action, map and call

--- src/containers/phones/index.js
+++ src/containers/phones/index.js
@@ -1,8 +1,25 @@
-import React from 'react'
-
-const Phones = () => (
-    <div>Phones</div>
-)
+import React, {Component} from 'react'
+import {connect} from 'react-redux';
+import {fetchPhones} from "../../actions";
 
 
-export default Phones
+
+class Phones extends Component {
+
+    componentDidMount() {
+        this.props.fetchPhones();
+    }
+
+    render() {
+        return (
+            <div>Phones</div>
+        )
+    }
+
+}
+
+const mapDispatchToProps = {
+    fetchPhones
+}
+
+export default connect(null, mapDispatchToProps)(Phones)
