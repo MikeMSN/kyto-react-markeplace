1e8e16e4db949fc25751cf5a757a7a8b10f86187
17543268 <Mosin.M.Yu@sberbank.ru>
2020-10-07 00:59:29 +0300

    added details on phone page

--- src/containers/phone/index.js
+++ src/containers/phone/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPhoneById} from "../../actions";
-
+import {getPhoneById} from "../../selectors";
+import * as R from 'ramda';
 
 class Phone extends React.Component {
     componentDidMount() {
@@ -9,14 +10,90 @@ class Phone extends React.Component {
     }
 
     render() {
+        const {phone} = this.props;
         return (
-            <div>Phone</div>
+            <div className='view-container'>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-md-9'>
+                            {phone && this.renderContent()}
+                        </div>
+                        <div className='col-md-3'>
+                            {phone && this.renderSideBar()}
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
+    }
+
+    renderContent() {
+        const {phone} = this.props;
+
+        return (
+            <div className='thumbnail'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <img className='img-thumbnail'
+                             src={phone.image}
+                             alt={phone.name}
+                        />
+                    </div>
+                    <div className='col-md-6'>
+                        {this.renderFields()}
+                    </div>
+                </div>
+                <div className='caption-full'>
+                    <h4 className='pull-right'>${phone.price}</h4>
+                    <h4 >${phone.name}</h4>
+                    <p>{phone.description}</p>
+                </div>
+            </div>
+        )
+    }
+
+    renderSideBar() {
+        return (
+            <div>Sidebar</div>
+        )
+    }
+
+    renderFields() {
+        const {phone} = this.props
+
+        const columnFields = R.compose(
+            R.toPairs,
+            R.pick([
+                'cpu',
+                'camera',
+                'size',
+                'weight',
+                'display',
+                'battery',
+                'memory'
+            ])
+        )(phone)
+
+        return columnFields.map(([key, value]) => (
+            <div className='column'>
+                <div className='ab-details-title'>
+                    <p>{key}</p>
+                </div>
+                <div className='ab-details-info'>
+                    {value}
+                </div>
+            </div>
+        ))
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        phone: getPhoneById(state, state.phonePage.id)
+    }
+}
 const mapDispatchToProps = {
     fetchPhoneById
 }
 
-export default connect(null, mapDispatchToProps)(Phone)
+export default connect(mapStateToProps, mapDispatchToProps)(Phone)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from "redux";
 import {connectRouter} from "connected-react-router";
 import phones from "./phones";
 import phonesPage from "./phonesPage";
+import phonePage from "./phonePage";
 
 export default history => combineReducers({
     phones,
     phonesPage,
+    phonePage,
     router: connectRouter(history)
 })
